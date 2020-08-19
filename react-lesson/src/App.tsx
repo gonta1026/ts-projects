fd3f8cd0c70d204b8885fb4e4a92ac42a15bcea7
gonta1026 <katano.original@gmail.com>
2020-08-20 07:29:05 +0900

    useState

--- react-lesson/src/App.tsx
+++ react-lesson/src/App.tsx
@@ -1,18 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Child from "./component/Child";
 
-
 const App: React.FC = () => {
+
+  const [count, setCount] = useState<number>(0);
+  const [message, setMessage] = useState<string>("");
+
+  const handleIncrement = (num: number) => {
+    setCount(num);
+  }
+  const handleDecrement = (num: number) => {
+    setCount(num);
+  }
+  // React.ChangeEvent<HTMLInputElement>
+  const handleMessage = (e: React.ChangeEvent<HTMLInputElement>): void => {
+    setMessage(e.target.value);
+  }
+
   return (
     <div className="App">
-      <Child message={"ss"} >
-        <section className="hoge">
-          Childだーー
-        </section>
-      </Child>
-    </div>
+      {count}
+      <button onClick={() => handleIncrement(count + 1)}>+ボタン</button>
+      <button onClick={() => handleDecrement(count - 1)}>-ボタン</button>
+      <input type="text" value={message} onChange={handleMessage} />
+    </div >
   );
 }
 
